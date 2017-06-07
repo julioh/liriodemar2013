932d2fdfc65e5d7f637d0eb033810291e4c78892
Julio Hernandez <juliochg@gmail.com>
2017-06-07 13:20:03 -0400

    Agregando Pagina

---
+++ newsletter.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="sp">
+ <head>
+ <meta charset="utf-8">
+ <title>··· Lirio de Mar ···</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="description" content="">
+ <meta name="author" content="gandalf" >
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+ <!-- Le styles --> 
+ <link href="administracion/bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="menu.css" rel="stylesheet">
+<link href="stylelirio1.css" rel="stylesheet">
+<style type="text/css">
+body {
+background-color:#f0dbac; 
+background-image: url("imagenes/pagecompletenewletter.jpg");
+}
+</style>
+</head>
+
+<body>
+
+<div class="container">
+
+
+<div class="newslettercompleta" id="newslettercompleta" style="display:none;">
+<div class="imagennewsletter">
+<img src="subidas/" alt="" class="imagen_newsletter_sola"/>
+</div>
+<div class="titulonewsletter">
+</div>
+<div class="cuerponewsletter">
+...
+</div>
+</div>    
+    
+    <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="administracion/bootstrap1/jquery.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-transition.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-alert.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-modal.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-dropdown.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-scrollspy.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-tab.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-tooltip.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-popover.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-button.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-collapse.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-carousel.js"></script>
+    <script src="administracion/bootstrap1/bootstrap-typeahead.js"></script>
+ <script type="text/javascript">
+$(document).ready(function(){
+  $(document).ready(function () {
+    $('#newslettercompleta').fadeIn(1200);
+  });
+});
+</script>
+  </body> 
+  
+</html>
